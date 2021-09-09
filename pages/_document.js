75baeda186f3fbd1a9c6c754eeed4ccef5c95a77
huks <huks7417@gmail.com>
2021-09-09 11:42:48 +0900

    style: add lint

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,11 @@
-import React from "react"
-import Document, { Html, Head, Main, NextScript } from "next/document"
-import { ServerStyleSheets } from "@material-ui/core/styles"
-import theme from "../src/theme"
+/* eslint-disable @next/next/no-document-import-in-page */
+/* eslint-disable react/display-name */
+import React from "react";
+import Document, { Html, Head, Main, NextScript } from "next/document";
+import { ServerStyleSheets } from "@material-ui/core/styles";
+import theme from "../src/theme";
 
-import { GA_TRACKING_ID } from "../libs/gtag"
+import { GA_TRACKING_ID } from "../libs/gtag";
 
 export default class MyDocument extends Document {
   render() {
@@ -39,7 +41,7 @@ export default class MyDocument extends Document {
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
 
@@ -69,15 +71,15 @@ MyDocument.getInitialProps = async (ctx) => {
   // 4. page.render
 
   // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets()
-  const originalRenderPage = ctx.renderPage
+  const sheets = new ServerStyleSheets();
+  const originalRenderPage = ctx.renderPage;
 
   ctx.renderPage = () =>
     originalRenderPage({
       enhanceApp: (App) => (props) => sheets.collect(<App {...props} />),
-    })
+    });
 
-  const initialProps = await Document.getInitialProps(ctx)
+  const initialProps = await Document.getInitialProps(ctx);
 
   return {
     ...initialProps,
@@ -86,5 +88,5 @@ MyDocument.getInitialProps = async (ctx) => {
       ...React.Children.toArray(initialProps.styles),
       sheets.getStyleElement(),
     ],
-  }
-}
+  };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,17 @@
-import React from 'react'
-import Container from '@material-ui/core/Container'
-import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-import ProTip from '../src/ProTip'
-import Link from '../src/Link'
-import Copyright from '../src/Copyright'
-import Gallery from '../src/Gallery'
-import KakaoMap from '../src/KakaoMap'
+import React from "react";
+import Container from "@material-ui/core/Container";
+import Typography from "@material-ui/core/Typography";
+import Box from "@material-ui/core/Box";
+import ProTip from "../src/ProTip";
+import Link from "../src/Link";
+import Copyright from "../src/Copyright";
+import Gallery from "../src/Gallery";
+import KakaoMap from "../src/KakaoMap";
 
 export default function Index() {
   return (
-    <Container sx={{ bgcolor: 'red' }}>
-      <Box sx={{ bgcolor: 'green' }} id="header">
+    <Container sx={{ bgcolor: "red" }}>
+      <Box sx={{ bgcolor: "green" }} id="header">
         <Typography variant="h4" component="h1" gutterBottom>
           november-twenty-eighth
         </Typography>
@@ -24,5 +24,5 @@ export default function Index() {
       <Gallery />
       <KakaoMap />
     </Container>
-  )
+  );
 }
