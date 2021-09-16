355531f8e183e211270605f45d73efcf17a40f76
huks <huks7417@gmail.com>
2021-09-16 13:57:17 +0900

    feat: add gtm

--- src/static/photos.js
+++ src/static/photos.js
@@ -1,12 +1,24 @@
+// const photos = [
+//   {
+//     imageUrl: './images/1.jpg',
+//   },
+//   {
+//     imageUrl: './images/2.jpg',
+//   },
+//   {
+//     imageUrl: './images/3.jpg',
+//   },
+// ]
+
 const photos = [
   {
-    imageUrl: './images/1.jpg',
+    imageUrl: './island1.jpg',
   },
   {
-    imageUrl: './images/2.jpg',
+    imageUrl: './island2.jpg',
   },
   {
-    imageUrl: './images/3.jpg',
+    imageUrl: './island3.jpg',
   },
 ]
 
