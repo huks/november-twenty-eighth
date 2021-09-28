d30ff960dad762854d6c29f2a6eca3749ad22cfc
huks <huks7417@gmail.com>
2021-09-28 12:23:25 +0900

    fix: update image paths

--- data/wedding.js
+++ data/wedding.js
@@ -43,8 +43,8 @@ const weddingInfo = {
     },
     kakaoPlaceId: 1726372096,
   },
-  imageUrl: './images/1.jpg',
-  secondaryImageUrl: './images/4.jpg',
+  imageUrl: '/images/1.jpg',
+  secondaryImageUrl: '/images/4.jpg',
   kakaoImageUrl:
     'https://lh3.googleusercontent.com/DBo2tVxLv5EI0cBvskFA_3tVGQcqZiMAjrl5jjyPckhFyiCDrfEUkshmIbjFaOMdG8uSOSrVIEbgLJn-9XGvQj9P7GoXlgzYqscdIZ2wUY2u_A_b1YbIYb9_oe9YGPkPd_xLJjegst0=w2400',
 }
