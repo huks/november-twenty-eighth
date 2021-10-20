13968c965959e21a9d86a5c426cb80d1d8df6437
huks <huks7417@gmail.com>
2021-10-20 13:44:40 +0900

    fix: fix clipboard copy

---
+++ src/utils/copyToClipboard.js
@@ -0,0 +1,19 @@
+const copyToClipboard = (textToCopy) => {
+  if (navigator.clipboard) {
+    // navigator clipboard api method
+    return navigator.clipboard.writeText(textToCopy)
+  } else {
+    // text area method
+    const tempEl = document.createElement('textarea')
+    tempEl.value = textToCopy
+    document.body.appendChild(tempEl)
+
+    tempEl.select()
+    return new Promise((res, rej) => {
+      document.execCommand('copy') ? res() : rej()
+      tempEl.remove()
+    })
+  }
+}
+
+export default copyToClipboard
