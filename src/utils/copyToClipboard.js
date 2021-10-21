311efb3c3e69e121b8333cb709d7b1410cd289fa
huks <huks7417@gmail.com>
2021-10-21 10:23:17 +0900

    fix: copyToClipboard use text area method only

--- src/utils/copyToClipboard.js
+++ src/utils/copyToClipboard.js
@@ -1,19 +1,32 @@
 const copyToClipboard = (textToCopy) => {
-  if (navigator.clipboard) {
-    // navigator clipboard api method
-    return navigator.clipboard.writeText(textToCopy)
-  } else {
-    // text area method
-    const tempEl = document.createElement('textarea')
-    tempEl.value = textToCopy
-    document.body.appendChild(tempEl)
+  console.log(`copyToClipboard(${textToCopy})`)
+  const tempEl = document.createElement('textarea')
+  tempEl.value = textToCopy
+  document.body.appendChild(tempEl)
 
-    tempEl.select()
-    return new Promise((res, rej) => {
-      document.execCommand('copy') ? res() : rej()
-      tempEl.remove()
-    })
-  }
+  tempEl.select()
+  return new Promise((res, rej) => {
+    document.execCommand('copy') ? res() : rej()
+    tempEl.remove()
+  })
 }
+
+// const copyToClipboard = (textToCopy) => {
+//   if (navigator.clipboard) {
+//     // navigator clipboard api method
+//     return navigator.clipboard.writeText(textToCopy)
+//   } else {
+//     // text area method
+//     const tempEl = document.createElement('textarea')
+//     tempEl.value = textToCopy
+//     document.body.appendChild(tempEl)
+
+//     tempEl.select()
+//     return new Promise((res, rej) => {
+//       document.execCommand('copy') ? res() : rej()
+//       tempEl.remove()
+//     })
+//   }
+// }
 
 export default copyToClipboard
